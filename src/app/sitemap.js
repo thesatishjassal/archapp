f55470827b487eeec7b2c930d239d66523a09131
Satish jassal <thesatishjassal@gmail.com>
2026-06-11 14:34:57 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://partners.panvik.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://partners.panvik.com/profile",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://partners.panvik.com/projects",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://partners.panvik.com/commissions",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+}
